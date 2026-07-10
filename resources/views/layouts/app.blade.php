76c70192b83a29f1d513db02dd7f5ef0220c63d6
vickoboy104-hub <support@belovedsubp.com>
2026-07-10 16:36:24 +0100

    Load final UI stability layer in portal layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/print.css'])
     @include('partials.theme-overrides')
     <link rel="stylesheet" href="{{ asset('portal-refresh.css') }}?v=20260706-overflow-fix">
+    <link rel="stylesheet" href="{{ asset('ui-stability.css') }}?v=20260710-ui-audit-1">
 </head>
 <body class="antialiased">
     <div class="site-page-shell app-shell min-h-screen">
