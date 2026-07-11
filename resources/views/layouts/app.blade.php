0b49d2dd2e05178376c925950efe79342a9f2112
vickoboy104-hub <support@belovedsubp.com>
2026-07-11 11:25:49 +0100

    Load sticky table usability styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     @include('partials.theme-overrides')
     <link rel="stylesheet" href="{{ asset('portal-refresh.css') }}?v=20260706-overflow-fix">
     <link rel="stylesheet" href="{{ asset('ui-stability.css') }}?v=20260710-ui-audit-1">
+    <link rel="stylesheet" href="{{ asset('table-usability.css') }}?v=20260711-sticky-actions-1">
 </head>
 <body class="antialiased">
     <div class="site-page-shell app-shell min-h-screen">
