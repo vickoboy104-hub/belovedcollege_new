6eee0f0b9e8c8a2e45352389fcc81f407c7fa1d4
vickoboy104-hub <support@belovedsubp.com>
2026-07-11 11:28:38 +0100

    Share cached settings once per request

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use App\Models\Setting;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\View\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,25 +21,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        view()->composer('*', function (View $view): void {
-            $settings = [];
+        $settings = Schema::hasTable('settings') ? Setting::allCached() : [];
 
-            if (Schema::hasTable('settings')) {
-                $settings = Setting::pluck('value', 'key')->all();
-            }
+        config([
+            'mail.default' => $settings['mail_mailer'] ?? config('mail.default'),
+            'mail.mailers.smtp.host' => $settings['mail_host'] ?? config('mail.mailers.smtp.host'),
+            'mail.mailers.smtp.port' => (int) ($settings['mail_port'] ?? config('mail.mailers.smtp.port')),
+            'mail.mailers.smtp.username' => $settings['mail_username'] ?? config('mail.mailers.smtp.username'),
+            'mail.mailers.smtp.password' => $settings['mail_password'] ?? config('mail.mailers.smtp.password'),
+            'mail.mailers.smtp.scheme' => $settings['mail_encryption'] ?? config('mail.mailers.smtp.scheme'),
+            'mail.from.address' => $settings['mail_from_address'] ?? $settings['school_email'] ?? config('mail.from.address'),
+            'mail.from.name' => $settings['mail_from_name'] ?? $settings['school_name'] ?? config('mail.from.name'),
+        ]);
 
-             config([
-                'mail.default' => $settings['mail_mailer'] ?? config('mail.default'),
-                'mail.mailers.smtp.host' => $settings['mail_host'] ?? config('mail.mailers.smtp.host'),
-                'mail.mailers.smtp.port' => (int) ($settings['mail_port'] ?? config('mail.mailers.smtp.port')),
-                'mail.mailers.smtp.username' => $settings['mail_username'] ?? config('mail.mailers.smtp.username'),
-                'mail.mailers.smtp.password' => $settings['mail_password'] ?? config('mail.mailers.smtp.password'),
-                'mail.mailers.smtp.scheme' => $settings['mail_encryption'] ?? config('mail.mailers.smtp.scheme'),
-                'mail.from.address' => $settings['mail_from_address'] ?? $settings['school_email'] ?? config('mail.from.address'),
-                'mail.from.name' => $settings['mail_from_name'] ?? $settings['school_name'] ?? config('mail.from.name'),
-            ]);
-
-            $view->with('schoolSettings', $settings);
-        });
+        view()->share('schoolSettings', $settings);
     }
 }
