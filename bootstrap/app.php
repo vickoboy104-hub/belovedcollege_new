6dda37a738c19ddf2444c0bd2a52edc8508e8f54
vickoboy104-hub <support@belovedsubp.com>
2026-07-13 09:56:18 +0100

    Enable application audit logging middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\ApplySecurityHeaders;
+use App\Http\Middleware\AuditUserActions;
 use App\Http\Middleware\DeferAutoplayMedia;
 use App\Http\Middleware\EnsureUserHasRole;
 use App\Http\Middleware\InjectReportPrintAssets;
@@ -37,6 +38,7 @@ return Application::configure(basePath: dirname(__DIR__))
             InjectReportPrintAssets::class,
             ApplySecurityHeaders::class,
             RequirePasswordChange::class,
+            AuditUserActions::class,
         ]);
 
         $middleware->alias([
