c4db1ea6ec1d56d21886adfb78af5ee595064e02
vickoboy104-hub <support@belovedsubp.com>
2026-08-19 09:44:47 +0100

    Register academic setup integrity guard

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,6 +9,7 @@ use App\Http\Middleware\InjectReportPrintAssets;
 use App\Http\Middleware\OptimizeUploadedMedia;
 use App\Http\Middleware\ProtectStaffAdministration;
 use App\Http\Middleware\RequirePasswordChange;
+use App\Http\Middleware\ValidateAcademicSetupInput;
 use App\Http\Middleware\ValidatePeopleManagementInput;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -43,6 +44,7 @@ return Application::configure(basePath: dirname(__DIR__))
             EnsureAccountIsActive::class,
             RequirePasswordChange::class,
             ValidatePeopleManagementInput::class,
+            ValidateAcademicSetupInput::class,
             ProtectStaffAdministration::class,
             AuditUserActions::class,
         ]);
