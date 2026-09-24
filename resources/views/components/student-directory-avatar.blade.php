d56eb97f5d9b60205d72695e68d894c1bca69fd3
vickoboy104-hub <support@belovedsubp.com>
2026-09-24 03:54:30 +0100

    Replace student-only avatar component with shared person avatar

--- resources/views/components/student-directory-avatar.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['user'])
-
-@if($user->avatar_url)
-    <img src="{{ $user->avatar_url }}" alt="{{ $user->fullName() }} profile photo" {{ $attributes->class(['table-avatar object-cover']) }} />
-@else
-    <div {{ $attributes->class(['table-avatar']) }}>
-        {{ substr($user->first_name, 0, 1) }}{{ substr($user->last_name, 0, 1) }}
-    </div>
-@endif
